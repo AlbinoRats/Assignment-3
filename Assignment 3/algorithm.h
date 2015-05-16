bbad07661cb0cd5f34e0b2a0e13a461c1ce4432c
Qiyuan Chen <45timesover@gmail.com>
2015-05-16 01:30:36 -0400

    MEDF
    implemtend minimum edit distance formula

---
+++ Assignment 3/algorithm.h
@@ -0,0 +1,19 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+#include<iostream>
+#include<string.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class algorithm{
+public:
+	algorithm(){
+
+	}
+	//MINIMUM EDIT DISTANCE FORMULA FROM WIKIPEDIA
+	unsigned int edit_distance(const string & s1, const string & s2);
+
+	//PUT PRIMS HERE
+
+};
+#endif
